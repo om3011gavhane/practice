f66434d685e0c23506312379086a991ce34ccae1
Om Gavhane <om2002gavhane@gmail.com>
2023-09-10 11:15:04 +0530

    after changes commit

--- practice.java
+++ practice.java
@@ -5,6 +5,7 @@ public class practice {
         int a=10;
         int b=20;
         int c=a+b;
+        System.out.println("hellow");
         System.out.println(a+b);
     }
 }
---
+++ sum.java
@@ -0,0 +1,9 @@
+package gitpractice;
+
+public class sum {
+    public static void main(String[] args) {
+        int a=21;
+        int c=456;
+        System.out.println(a+c);
+    }
+}
